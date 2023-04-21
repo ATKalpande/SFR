64a8f3e9663253a2d821154bed797cc6b8aa3bf2
Akash T KALPANDE <88917542+ATKalpande@users.noreply.github.com>
2023-04-21 16:50:43 +0530

    add component about
    About us

---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='text-red-200'>About</div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Footer, Navbar } from "@/components";
+import { Footer, About, Navbar  } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <main className="min-h-screen py-5">
       <Navbar />
+      <About />
       <Footer />
     </main>
   );
