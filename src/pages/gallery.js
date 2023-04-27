79473a933f1bde7e2e6861701abc2a0cafb2ce69
Akash T KALPANDE <88917542+ATKalpande@users.noreply.github.com>
2023-04-28 02:03:06 +0530

    update gallery
    Make hover width full

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -4,112 +4,113 @@ import React from "react";
 export const gallery = () => {
   const galleryImg = `my-3 my-8 px-8 py-8 flex justify-center   align-center`
   const mrgin= `mx-8`
+  const hovimg =  `hover:w-full`
   return (
 
     <Layout>
       <>
         <section className={galleryImg}>
           <div className={mrgin}>
-                <img width={200} src="galleryPhoto/1.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/1.jpeg" alt="" />
                 </div>
               <div className={mrgin}>
-              <img width={200} src="galleryPhoto/2.jpeg" alt="" />
+              <img className={hovimg} width={200} src="galleryPhoto/2.jpeg" alt="" />
               </div>
              <div className={mrgin}>
-                <img width={200} src="galleryPhoto/27.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/27.jpeg" alt="" />
                 </div>
                 <div className={mrgin}>
-                <img width={200} src="galleryPhoto/4.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/4.jpeg" alt="" />
                 </div>
         </section>
 
       <div className={galleryImg}>
       <div className={mrgin}>
-                <img width={200} src="galleryPhoto/5.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/5.jpeg" alt="" />
                 </div>
               <div className={mrgin}>
-              <img width={200} src="galleryPhoto/6.jpeg" alt="" />
+              <img className={hovimg} width={200} src="galleryPhoto/6.jpeg" alt="" />
               </div>
              <div className={mrgin}>
-                <img width={200} src="galleryPhoto/7.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/7.jpeg" alt="" />
                 </div>
                 <div className={mrgin}>
-                <img width={200} src="galleryPhoto/43.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/43.jpeg" alt="" />
                 </div>
       </div>
 
       <div className={galleryImg}>
       <div className={mrgin}>
-                <img width={200} src="galleryPhoto/9.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/9.jpeg" alt="" />
                 </div>
               <div className={mrgin}>
-              <img width={200} src="galleryPhoto/10.jpeg" alt="" />
+              <img className={hovimg} width={200} src="galleryPhoto/10.jpeg" alt="" />
               </div>
              <div className={mrgin}>
-                <img width={200} src="galleryPhoto/11.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/11.jpeg" alt="" />
                 </div>
                 <div className={mrgin}>
-                <img width={200} src="galleryPhoto/12.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/12.jpeg" alt="" />
                 </div>
       </div>
 
       <div className={galleryImg}>
       <div className={mrgin}>
-                <img width={200} src="galleryPhoto/13.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/13.jpeg" alt="" />
                 </div>
               <div className={mrgin}>
-              <img width={200} src="galleryPhoto/14.jpeg" alt="" />
+              <img className={hovimg} width={200} src="galleryPhoto/14.jpeg" alt="" />
               </div>
              <div className={mrgin}>
-                <img width={200} src="galleryPhoto/15.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/15.jpeg" alt="" />
                 </div>
                 <div className={mrgin}>
-                <img width={200} src="galleryPhoto/16.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/16.jpeg" alt="" />
                 </div>
       </div>
 
       <div className={galleryImg}>
       <div className={mrgin}>
-                <img width={200} src="galleryPhoto/17.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/17.jpeg" alt="" />
                 </div>
               <div className={mrgin}>
-              <img width={200} src="galleryPhoto/18.jpeg" alt="" />
+              <img className={hovimg} width={200} src="galleryPhoto/18.jpeg" alt="" />
               </div>
              <div className={mrgin}>
-                <img width={200} src="galleryPhoto/19.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/19.jpeg" alt="" />
                 </div>
                 <div className={mrgin}>
-                <img width={200} src="galleryPhoto/20.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/20.jpeg" alt="" />
                 </div>
       </div>
 
       <div className={galleryImg}>
       <div className={mrgin}>
-                <img width={200} src="galleryPhoto/21.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/21.jpeg" alt="" />
                 </div>
               <div className={mrgin}>
-              <img width={200} src="galleryPhoto/22.jpeg" alt="" />
+              <img className={hovimg} width={200} src="galleryPhoto/22.jpeg" alt="" />
               </div>
              <div className={mrgin}>
-                <img width={200} src="galleryPhoto/23.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/23.jpeg" alt="" />
                 </div>
                 <div className={mrgin}>
-                <img width={200} src="galleryPhoto/24.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/24.jpeg" alt="" />
                 </div>
       </div>
 
       <div className={galleryImg}>
       <div className={mrgin}>
-                <img width={200} src="galleryPhoto/25.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/25.jpeg" alt="" />
                 </div>
               <div className={mrgin}>
-              <img width={200} src="galleryPhoto/26.jpeg" alt="" />
+              <img className={hovimg} width={200} src="galleryPhoto/26.jpeg" alt="" />
               </div>
              <div className={mrgin}>
-                <img width={200} src="galleryPhoto/3.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/3.jpeg" alt="" />
                 </div>
                 <div className={mrgin}>
-                <img width={200} src="galleryPhoto/28.jpeg" alt="" />
+                <img className={hovimg} width={200} src="galleryPhoto/28.jpeg" alt="" />
                 </div>
       </div>
 
