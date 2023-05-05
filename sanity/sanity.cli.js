860e04f0df5fc07104babffdd9854cc64354df40
rishikesh.s <rdshinde2000@gmail.com>
2023-05-05 14:39:35 +0530

    feat: sanity homepage schema

---
+++ sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'bcfb3044',
+    dataset: 'production',
+  },
+})
---
+++ sanity/schemas/homePage.js
@@ -0,0 +1,114 @@
+export const homePage = {
+  type: 'document',
+  name: 'homepage',
+  title: 'Homepage',
+  fields: [
+    {
+      type: 'array',
+      name: 'sections',
+      title: 'Sections',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {name: 'heading', type: 'string', title: 'Heading'},
+            {name: 'subheading', type: 'string', title: 'Subheading'},
+            {name: 'description', type: 'array', title: 'Description', of: [{type: 'block'}]},
+            {
+              name: 'media',
+              type: 'object',
+              title: 'Media',
+              fields: [
+                {
+                  name: 'loadSingleItem',
+                  type: 'boolean',
+                  title: 'Load a single item',
+                  description: 'If checked, only one image/video can be added to this section.',
+                  initialValue: false,
+                },
+                {
+                  name: 'useGridDisplay',
+                  type: 'boolean',
+                  title: 'Use grid display',
+                  description: 'If checked, images/videos will be displayed in a grid layout.',
+                  initialValue: false,
+                },
+                {
+                  name: 'useCarouselDisplay',
+                  type: 'boolean',
+                  title: 'Use carousel display',
+                  description: 'If checked, images/videos will be displayed in a carousel.',
+                  initialValue: false,
+                },
+                {
+                  name: 'items',
+                  type: 'array',
+                  title: 'Items',
+                  of: [
+                    {
+                      name: 'image',
+                      type: 'image',
+                      title: 'Image',
+                      options: {
+                        hotspot: true,
+                      },
+                    },
+                    {
+                      name: 'video',
+                      type: 'object',
+                      title: 'Video',
+                      fields: [
+                        {
+                          name: 'url',
+                          type: 'url',
+                          title: 'URL',
+                        },
+                        {
+                          name: 'title',
+                          type: 'string',
+                          title: 'Title',
+                          description: 'Optional: add a title for the video',
+                        },
+                        {
+                          name: 'description',
+                          type: 'text',
+                          title: 'Description',
+                          description: 'Optional: add a description for the video',
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              name: 'richTextContents',
+              type: 'array',
+              title: 'Main Content',
+              of: [{type: 'block'}],
+            },
+            {
+              name: 'bulletPoints',
+              type: 'array',
+              title: 'Bullet Points',
+              of: [
+                {
+                  type: 'object',
+                  fields: [
+                    {name: 'title', type: 'string', title: 'Title'},
+                    {
+                      name: 'items',
+                      type: 'array',
+                      title: 'Items',
+                      of: [{type: 'block'}],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
