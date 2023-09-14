7c0d3160fbf28f2607801128be13e74cf0f4b2f5
rishikesh.s <rdshinde2000@gmail.com>
2023-09-14 23:26:23 +0530

    feat: homepage schema added

--- sanity/schemas/homePage.js
+++ sanity/schemas/homePage.js
@@ -2,120 +2,73 @@ export const homePage = {
   type: 'document',
   name: 'homepage',
   title: 'Homepage',
+
   fields: [
     {
-      type: 'array',
-      name: 'sections',
-      title: 'Sections',
-      of: [
+      name: 'heroComponent',
+      title: 'Hero Component',
+      type: 'object',
+      fields: [
         {
-          type: 'object',
-          fields: [
-            {name: 'heading', type: 'string', title: 'Heading'},
-            {name: 'subheading', type: 'string', title: 'Subheading'},
-            {name: 'description', type: 'array', title: 'Description', of: [{type: 'block'}]},
+          name: 'title',
+          title: 'Title',
+          type: 'string',
+        },
+        {
+          name: 'paragraphs',
+          title: 'Paragraphs',
+          type: 'array',
+          of: [{type: 'block'}],
+        },
+      ],
+    },
+    {
+      name: 'imageSliderComponent',
+      title: 'Image Slider Component',
+      type: 'object',
+      fields: [
+        {
+          name: 'images',
+          title: 'Images',
+          type: 'array',
+          of: [
             {
-              name: 'media',
               type: 'object',
-              title: 'Media',
               fields: [
                 {
-                  name: 'loadSingleItem',
-                  type: 'boolean',
-                  title: 'Display a single item',
-                  description:
-                    'If checked, only first image/video can be added to this section from the list.',
-                  initialValue: false,
+                  name: 'image',
+                  title: 'Image',
+                  type: 'image',
+                  options: {
+                    hotspot: true,
+                  },
                 },
                 {
-                  name: 'useGridDisplay',
-                  type: 'boolean',
-                  title: 'Use grid display',
-                  description: 'If checked, images/videos will be displayed in a grid layout.',
-                  initialValue: false,
+                  name: 'altText',
+                  title: 'Alt Text',
+                  type: 'string',
                 },
                 {
-                  name: 'useCarouselDisplay',
-                  type: 'boolean',
-                  title: 'Use carousel display',
-                  description: 'If checked, images/videos will be displayed in a carousel.',
-                  initialValue: false,
-                },
-                {
-                  name: 'items',
-                  type: 'array',
-                  title: 'Items',
-                  of: [
-                    {
-                      name: 'image',
-                      type: 'image',
-                      title: 'Image',
-                      options: {
-                        hotspot: true,
-                      },
-                    },
-                    {
-                      name: 'video',
-                      type: 'object',
-                      title: 'Video',
-                      fields: [
-                        {
-                          type: 'file',
-                          accept: 'video/*',
-                          title: 'Video',
-                          name: 'video',
-                          description: 'Upload a video file.',
-                        },
-                        {
-                          name: 'url',
-                          type: 'url',
-                          title: 'URL',
-                          description: 'Optional: add a URL for the video.',
-                        },
-                        {
-                          name: 'title',
-                          type: 'string',
-                          title: 'Title',
-                          description: 'Optional: add a title for the video.',
-                        },
-                        {
-                          name: 'description',
-                          type: 'text',
-                          title: 'Description',
-                          description: 'Optional: add a description for the video.',
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            },
-            {
-              name: 'richTextContents',
-              type: 'array',
-              title: 'Main Content',
-              of: [{type: 'block'}],
-            },
-            {
-              name: 'bulletPoints',
-              type: 'array',
-              title: 'Bullet Points',
-              of: [
-                {
-                  type: 'object',
-                  fields: [
-                    {name: 'title', type: 'string', title: 'Title'},
-                    {
-                      name: 'items',
-                      type: 'array',
-                      title: 'Items',
-                      of: [{type: 'block'}],
-                    },
-                  ],
+                  name: 'title',
+                  title: 'Title',
+                  type: 'string',
                 },
               ],
             },
           ],
+        },
+      ],
+    },
+    {
+      name: 'achievementsComponent',
+      title: 'Achievements Component',
+      type: 'object',
+      fields: [
+        {
+          name: 'achievements',
+          title: 'Achievements',
+          type: 'array',
+          of: [{type: 'block'}],
         },
       ],
     },
